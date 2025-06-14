1f0b2e953aab3e6857ec9ea623a09da22241f040
kartik <59840735+karthikg664@users.noreply.github.com>
2025-06-14 15:42:56 +0530

    completed the logic to get the computer choice.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello World");
+function getComputerChoice(min,max) {
+    const minCeiled=Math.ceil(min);
+    const maxFloored=Math.floor(max);
+
+    const choice=Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+
+    if (choice === 1) {
+        return "Rock";
+    } else if (choice === 2) {
+        return "Paper";
+    } else {
+        return "scissors"
+    }
+}
+console.log(getComputerChoice(0,4));
